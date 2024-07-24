4465807adc225ff7aabccf87509a8cecfba90b8b
Monalisa Onyango <+>
2024-07-24 16:10:01 +0300

    behaviour lab

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+
+class Cat{
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+
+  // For an instance of Cat, speak returns "name says meow!",
+  speak(){
+    return `${this.name} says meow!`
+  }
+  
+}
+let littleKitty= new Cat('meowmeow')
+   console.log(littleKitty.speak())
+// .....................................................................................................
+   
+
+class Dog{
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+
+    speak(){
+        return `${this.name} says woof!`
+      }
+}
+let myDog= new Cat('Bosco')
+   console.log(myDog.speak())
+
+// ......................................................................................................
+
+
+class Bird{
+    constructor(name,sex){
+        this.name=name;
+        this.sex=sex;
+    }
+    
+  speak(){
+
+    if(this.sex === 'male'){
+        return `It's me! ${this.name}, the parrot!`
+    } else {
+        return `${this.name} says squawk!`
+    }
+  }
+}
+
+let birdGender= new Bird('Tweety','female')
+ console.log(birdGender.speak())
+
+
+
